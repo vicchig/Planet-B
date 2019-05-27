cf0cf3ade994c1c8ffab631b761ea18f16ee7869
vic <vchigir12@gmail.com>
2019-05-27 20:34:42 +0000

    Water vapour now appears only after you have freed all the water

---
+++ Assets/Scripts/VapourController.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VapourController : MonoBehaviour
+{
+    void Start()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            transform.GetChild(i).GetComponent<ParticleSystem>().Stop();
+        }
+    }
+}
+    
--- Assets/Scripts/WaterEvaporator.cs
+++ Assets/Scripts/WaterEvaporator.cs
@@ -4,34 +4,17 @@ using UnityEngine;
 
 public class WaterEvaporator : MonoBehaviour
 {
-    private bool evaporate;
-    private float temperature;//current temperature of the water ball
-    public float evaporationTemp;
-
-    private void Start()
-    {
-        evaporate = false;
-    }
-
-    private void Update()
-    {
-        if (evaporate == true)
-        {
-            temperature += 1;
-        }
-
-        if (temperature > evaporationTemp)
-        {
-            evaporate = false;
-            Destroy(gameObject);
-        }
-    }
+    
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.transform.tag == "EvapArea")
         {
-            evaporate = true;
+            GameObject evaporator = GameObject.Find("VapourController");
+
+            for (int i = 0; i < evaporator.transform.childCount; i++) {
+                evaporator.transform.GetChild(i).GetComponent<ParticleSystem>().Play();
+            }
         }
     }
 }
