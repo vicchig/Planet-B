86daa850423b18f2f4bce07e6a2de41c15437e65
vic <vchigir12@gmail.com>
2019-06-05 15:42:26 +0000

    Added particle systems to lava

--- Assets/Scripts/WaterPourController.cs
+++ Assets/Scripts/WaterPourController.cs
@@ -4,17 +4,20 @@ using UnityEngine;
 
 public class WaterPourController : MonoBehaviour
 {
-
+    [Header("Attributes")]
     public GameObject particleSource;
+    public float decrementTime;
 
     private PlayerAttributes attributes;
     private bool fPressed;
     private ParticleGenerator generator;
+    private float pourTime;
     private void Start()
     {
         attributes = GetComponent<PlayerAttributes>();
         generator = particleSource.GetComponent<ParticleGenerator>();
         generator.enabled = false;
+        pourTime = 0;
     }
 
     private void Update()
@@ -23,7 +26,9 @@ public class WaterPourController : MonoBehaviour
         {
             generator.enabled = true;
             generator.particleForce = new Vector3(150 * this.GetComponent<PlayerMovement>().dir, -20, 0);
-            
+            if (pourTime >= decrementTime) {
+                attributes.SetCurrentWater(attributes.GetCurrentWater() - 1);
+            }
         }
         else{
             generator.enabled = false;
@@ -37,6 +42,21 @@ public class WaterPourController : MonoBehaviour
         }
         else if (Input.GetButtonUp("Flood")) {
             fPressed = false;
+        }
+
+        if (fPressed)
+        {
+            if (pourTime < decrementTime)
+            {
+                pourTime += Time.deltaTime;
+            }
+            else
+            {
+                pourTime = 0;
+            }
+        }
+        else {
+            pourTime = 0;
         }
     }
 
