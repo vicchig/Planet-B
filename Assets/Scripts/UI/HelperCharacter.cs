a761d37679ff347eba739dccb7f9fa1e6bec8bf0
vic <vchigir12@gmail.com>
2019-06-06 21:06:17 +0000

    Made echo work on a queue based system to eliminate most of the code we had before.

--- Assets/Scripts/UI/HelperCharacter.cs
+++ Assets/Scripts/UI/HelperCharacter.cs
@@ -16,12 +16,22 @@ public class HelperCharacter : MonoBehaviour
     public int level = 1;
 
     [Header("Sounds")]
-    public AudioClip intro1;
-    public AudioClip intro2;
-    public AudioClip intro3;
+    public AudioClip introClip;
+    public AudioClip objectiveLevelClip1_0;
+    public AudioClip objectiveLevelClip1_1;
+    public AudioClip objectiveLevelClip1_2;
+    public AudioClip objectiveLevelClip1_3;
+    public AudioClip objectiveLevelClip1_4;
+    public AudioClip objectiveLevelClip1_5;
+    public AudioClip objectiveLevelClip1_6;
     public AudioClip airFound;
     public AudioClip waterFound;
     public AudioClip destructibleFound;
+    public AudioClip waterPoolFoundClip;
+    public AudioClip butterlfyCommentClip;
+    public AudioClip rockWarningClip;
+    public AudioClip aboutToDieClip;
+    public AudioClip lavaCommentClip;
 
     //text mesh where the text will be displayed
     private TextMeshProUGUI uiTextMesh;
@@ -29,10 +39,6 @@ public class HelperCharacter : MonoBehaviour
     //how long the current text has been displayed for so far
     private float textTimer;
 
-    //Begin Game
-    private string intro = "Hello, I am Echo, the built-in A.I. of your suit. I hope we can get along. If not, please remember that I control your suit's life support systems.";
-    private string objectiveLevel1_0 = "This planet looks nice. Unfortunately, the water cycle is completely non-existent. If we want to colonize it, you will have to fix the water cycle.";
-    private string objectiveLevel1_1 = "The first step would be to evaporate some water into the atmosphere using heat energy. There does not seem to be any water on the surface. Maybe we should investigate the cave system below us.";
     private int cycle;
     private bool showIntro;
 
@@ -41,9 +47,26 @@ public class HelperCharacter : MonoBehaviour
     ObjectText waterDropTxt;
     ObjectText destructibleTxt;
 
-    //whether the display is already dispalying any text
-    private bool isBusy; 
+    ObjectText introTxt;
+    ObjectText objectiveLevelTxt1_0;
+    ObjectText objectiveLevelTxt1_1;
+    ObjectText objectiveLevelTxt1_2;
+    ObjectText objectiveLevelTxt1_3;
+    ObjectText objectiveLevelTxt1_4;
+    ObjectText objectiveLevelTxt1_5;
+    ObjectText objectiveLevelTxt1_6;
 
+    ObjectText waterPoolFoundTxt;
+
+    ObjectText butterlfyCommentTxt;
+    ObjectText rockWarningTxt;
+    ObjectText aboutToDieTxt;
+    ObjectText lavaCommentTxt;
+
+    //whether the display is already dispalying any text
+    private bool isBusy;
+
+    private Queue<ObjectText> sounds;
 
     private void Start()
     {
@@ -55,97 +78,50 @@ public class HelperCharacter : MonoBehaviour
         cycle = 1;
         showIntro = true;
 
-        airSourceTxt = new ObjectText("This weird orb seems to be the only source of breathable air in this cave. Better grab it to replenish the air supply.", false);
-        waterDropTxt = new ObjectText("Look! We seem to have found some water. Better collect it.", false);
-        destructibleTxt = new ObjectText("The ground in the vicinity seems to be prone to collapse. Try shooting it with your weapon.", false);
+        airSourceTxt = new ObjectText("This weird orb seems to be the only source of breathable air in this cave. Better grab it to replenish the air supply.", false, 4, airFound, 1);
+        waterDropTxt = new ObjectText("Look! We seem to have found some water. Better collect it.", false, 4, waterFound, 1);
+        destructibleTxt = new ObjectText("The ground in the vicinity seems to be prone to collapse. Try shooting it with your weapon.", false, 4, destructibleFound, 2);
+
+        introTxt = new ObjectText("Hello, I am Echo, the built-in A.I. of your suit. I hope we can get along. If not, please remember that I control your suit's life support systems.", false, 4, introClip, 1);
+        objectiveLevelTxt1_0 = new ObjectText("This planet looks nice. Unfortunately, the water cycle is completely non-existent. If we want to colonize it, you will have to fix the water cycle.", false, objectiveLevelClip1_0.length, objectiveLevelClip1_0, 1);
+        objectiveLevelTxt1_1 = new ObjectText("The first step would be to evaporate some water into the atmosphere using heat energy. There does not seem to be any water on the surface. Maybe we should investigate the cave system below us.", false, objectiveLevelClip1_1.length, objectiveLevelClip1_1, 1);
+        objectiveLevelTxt1_2 = new ObjectText("By my calculations we will need about 9 of these to have enough water to evaporate.", false, 4, objectiveLevelClip1_2, 1);
+        objectiveLevelTxt1_3 = new ObjectText("We have enough water now. We should find a way back to the surface and see if there is any area that we can use to create a lake.", false, 4, objectiveLevelClip1_3, 1);
+        objectiveLevelTxt1_4 = new ObjectText("We should probably explore some more to the east.", false, 4, objectiveLevelClip1_4, 1);
+        objectiveLevelTxt1_5 = new ObjectText("This should be enough. Now we just need to evaporate it. Unfortunately, this planet's Sun is too weak to do that. We will have to collect heat from it and amplify it.", false, 4, objectiveLevelClip1_5, 1);
+        objectiveLevelTxt1_6 = new ObjectText("Remember that your weapon mode 2 amplifies heat energy.", false, 4, objectiveLevelClip1_6, 1);
+
+        waterPoolFoundTxt = new ObjectText("This looks like a good spot to release our water. Remember, you can do that by holding down F.", false, 4, waterPoolFoundClip, 1);
+
+        butterlfyCommentTxt = new ObjectText("Wow. Aren't these beautiful!", false, 4, butterlfyCommentClip, 1);
+        rockWarningTxt = new ObjectText("The ceiling of the cave in this area is prone to collapse. Watch your head!", false, 4, rockWarningClip, 1);
+        aboutToDieTxt = new ObjectText("Warning: operator sustaining critical damage.", false, 4, aboutToDieClip, 1);
+        lavaCommentTxt = new ObjectText("Better not stay in here for too long, your suit won't be able to take this much heat.", false, 4, lavaCommentClip, 1);
+
+        sounds = new Queue<ObjectText>();
+        sounds.Enqueue(objectiveLevelTxt1_0);
+        sounds.Enqueue(objectiveLevelTxt1_1);
     }
 
     private void Update()
     {
-        //hint text and intros
-        if (level == 1) {
-            isBusy = true;
-            if (showIntro && cycle == 1)
-            {
-                textDuration = intro1.length;
-                AudioManager.playClip(intro1);
-                uiTextMesh.text = intro;
-                if (textTimer + 1.9 >= textDuration)
-                {
-                    cycle++;
-                    textTimer = 0;
-                }
-            }
-            else if (showIntro && cycle == 2)
-            {
-                uiTextMesh.text = objectiveLevel1_0;
-                textDuration = intro2.length;
-                AudioManager.playClip(intro2);
-                if (textTimer - 0.3 >= textDuration)
-                {
-                    cycle++;
-                    textTimer = 0;
-                }
-            }
-            else if (showIntro && cycle == 3)
-            {
-                uiTextMesh.text = objectiveLevel1_1;
-                textDuration = intro3.length;
-                AudioManager.playClip(intro3);
-                if (textTimer   >= textDuration)
-                {
-                    cycle++;
-                    textTimer = 0;
-                }
-            }
-            else {
-                showIntro = false;
-                isBusy = false;
-                textDuration = 4;
-            }
-        }
-
-        //in-game object descriptions
-        if (textTimer + 1 <= textDuration) {
-            if (!isBusy) {
-                if (airSourceTxt.isShowText())
-                {
-
-                    airSourceTxt.setTextShows(airSourceTxt.getTextShows() + 1);
-                    uiTextMesh.text = airSourceTxt.getText();
-                    textDuration = airFound.length;
-                    AudioManager.playClip(airFound);
-
-                    isBusy = true;
-                }
-                else if (waterDropTxt.isShowText())
-                {
-                    waterDropTxt.setTextShows(waterDropTxt.getTextShows() + 1);
-                    uiTextMesh.text = waterDropTxt.getText();
-                    textDuration = waterFound.length;
-                    AudioManager.playClip(waterFound);
-                    isBusy = true;
-                }
-                else if (destructibleTxt.isShowText())
-                {
-                    destructibleTxt.setTextShows(destructibleTxt.getTextShows() + 1);
-                    uiTextMesh.text = destructibleTxt.getText();
-                    textDuration = destructibleFound.length;
-                    AudioManager.playClip(destructibleFound);
-                    isBusy = true;
-                }
-            }
-        }
-        else
+        //playing sounds
+        if (sounds.Count >= 1 && !isBusy)
         {
-            airSourceTxt.setShowText(false);
-            waterDropTxt.setShowText(false);
-            destructibleTxt.setShowText(false);
-            isBusy = false;
+            ObjectText currText = null;
+            currText = sounds.Dequeue();
+
+            if (currText.getTextShows() < currText.getMaxTextShows()) {
+                isBusy = true;
+                textDuration = currText.getDuration();
+                currText.setTextShows(currText.getTextShows() + 1);
+                uiTextMesh.text = currText.getText();
+                AudioManager.playClip(currText.getClip());
+            }
         }
 
-
-        if (!airSourceTxt.isShowText() && !waterDropTxt.isShowText() && !destructibleTxt.isShowText() && !showIntro )
+        //reseting the text mesh
+        if (!isBusy)
         {
             uiTextMesh.text = "";
             txtBackground.GetComponent<Image>().enabled = false;
@@ -155,47 +131,57 @@ public class HelperCharacter : MonoBehaviour
             txtBackground.GetComponent<Image>().enabled = true;
             portraitObj.GetComponent<Image>().enabled = true;
         }
+
+        //enqueuing new sounds
     }
 
     private void FixedUpdate()
     {
-        if (airSourceTxt.isShowText() || waterDropTxt.isShowText() || destructibleTxt.isShowText() || showIntro )
+        Debug.Log("Duration: " +textDuration);
+        Debug.Log("Timer: " + textTimer);
+
+        //update timer
+        if (isBusy)
         {
+            if (textTimer >= textDuration) {
+                isBusy = false;
+                textTimer = 0;
+            }
             textTimer += Time.deltaTime;
-        }
-        else {
-            textTimer = 0;
         }
     }
 
     private void OnTriggerStay2D(Collider2D collision)
     {
-        if (!isBusy) {
-            if (collision.tag == "AirSourceTextArea" && airSourceTxt.getTextShows() < 1)
-            {
-                airSourceTxt.setShowText(true);
-            }
-            else if (collision.tag == "WaterTextArea" && waterDropTxt.getTextShows() < 1)
-            {
-                waterDropTxt.setShowText(true);
-            }
-            else if (collision.tag == "DestructibleTextArea" && destructibleTxt.getTextShows() < 2)
-            {
-                destructibleTxt.setShowText(true);
-                collision.GetComponent<BoxCollider2D>().enabled = false;
-            }
+        if (collision.tag == "AirSourceTextArea" && airSourceTxt.getTextShows() < 1)
+        {
+            sounds.Enqueue(airSourceTxt);
         }
-        
+        else if (collision.tag == "WaterTextArea" && waterDropTxt.getTextShows() < 1)
+        {
+            sounds.Enqueue(waterDropTxt);
+        }
+        else if (collision.tag == "DestructibleTextArea" && destructibleTxt.getTextShows() < 2 && !isBusy)
+        {
+            sounds.Enqueue(destructibleTxt);
+            collision.GetComponent<BoxCollider2D>().enabled = false;
+        }
     }
 
     private class ObjectText {
         private bool showText; //whether this text should be shown right now or not
         private int textShows; //number of times this text has been shown
         private string text; //text to be shown
+        private float duration;
+        private int maxTextShows;
+        private AudioClip clip;
 
-        public ObjectText(string text, bool showText) {
+        public ObjectText(string text, bool showText, float duration, AudioClip clip, int maxTextShows) {
             this.text = text;
             this.showText = showText;
+            this.duration = duration;
+            this.clip = clip;
+            this.maxTextShows = maxTextShows;
             textShows = 0;
         }
 
@@ -217,6 +203,18 @@ public class HelperCharacter : MonoBehaviour
         }
         public void setText(string text) {
             this.text = text;
+        }
+        public void setDuration(float duration) {
+            this.duration = duration;
+        }
+        public float getDuration() {
+            return duration;
+        }
+        public AudioClip getClip() {
+            return clip;
+        }
+        public int getMaxTextShows() {
+            return maxTextShows;
         }
     }
 }
