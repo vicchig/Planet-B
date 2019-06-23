541d67bad1d32d9b12a56a1484c10604973eedd7
vic <vchigir12@gmail.com>
2019-06-23 17:30:04 +0000

    Made the UIManager general. Now it will store all the bars that we need, but we will only activate it in the appropriate level scripts.
    Also changed the UI in the Tutorial to match the Level 1 UI.

--- Assets/Scripts/UI/UIManager.cs
+++ Assets/Scripts/UI/UIManager.cs
@@ -6,50 +6,35 @@ using UnityEngine.SceneManagement;
 
 public class UIManager : MonoBehaviour
 {
+    [Header("Inherited")]
     public GameObject player;
     public GameObject managerObj;
 
-    private PlayerAttributes playerAttributes;
-    private GameManagerLevel1 manager;
+    protected PlayerAttributes playerAttributes;
 
-    private ProgressBar healthBar;
-    private ProgressBar heatBar;
-    private ProgressBar waterInPoolBar;
+    protected ProgressBar healthBar;
+    protected ProgressBar heatBar;
+    protected ProgressBar waterInPoolBar;
 
-    void Start()
+    protected virtual void Start()
     {
         playerAttributes = player.GetComponent<PlayerAttributes>();
-        manager = managerObj.GetComponent<GameManagerLevel1>();
 
         healthBar = this.transform.GetChild(0).GetComponent<ProgressBar>();
         setInitialBarValues(healthBar, playerAttributes.maxHealth, playerAttributes.maxHealth);
 
-        //WaterCount - child 2
-        this.transform.GetChild(1).GetComponent<Text>().text = ":" + playerAttributes.waterCollected;
-
         heatBar = this.transform.GetChild(2).GetComponent<ProgressBar>();
         setInitialBarValues(heatBar, playerAttributes.getCurrentHeat(), playerAttributes.maxHeat);
-
-        if (SceneManager.GetActiveScene().name == "Level 1") {
-            waterInPoolBar = this.transform.GetChild(3).GetComponent<ProgressBar>();
-            setInitialBarValues(waterInPoolBar, manager.getAmountOfWaterInPool(), manager.waterNeededInPool);
-        }
-        
     }
 
-    private void Update()
+    protected virtual void Update()
     {
         healthBar.BarValue = playerAttributes.GetCurrentHealth();
         this.transform.GetChild(1).GetComponent<Text>().text = ":" + playerAttributes.GetCurrentWater();
         heatBar.BarValue = playerAttributes.getCurrentHeat();
-        if (SceneManager.GetActiveScene().name == "Level 1")
-        {
-            waterInPoolBar.BarValue = manager.getAmountOfWaterInPool();
-
-        }
     }
 
-    private void setInitialBarValues(ProgressBar bar, int curr, int max) {
+    protected void setInitialBarValues(ProgressBar bar, int curr, int max) {
         bar.maxValue = max;
         bar.BarValue = curr;
     }
---
+++ Assets/Scripts/UI/UIManagerL1.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManagerL1 : UIManager
+{
+    private GameManagerLevel1 manager;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        manager = managerObj.GetComponent<GameManagerLevel1>();
+
+        //WaterCount - child 2
+        this.transform.GetChild(1).GetComponent<Text>().text = ":" + playerAttributes.waterCollected;
+
+        waterInPoolBar = this.transform.GetChild(3).GetComponent<ProgressBar>();
+        setInitialBarValues(waterInPoolBar, manager.getAmountOfWaterInPool(), manager.waterNeededInPool);
+
+    }
+
+
+    protected override void Update()
+    {
+        base.Update();
+
+        waterInPoolBar.BarValue = manager.getAmountOfWaterInPool();
+
+    }
+}
