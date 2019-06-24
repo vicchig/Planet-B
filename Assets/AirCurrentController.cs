b4fc871a699306c4d857370067ef3b6ab6695f03
vic <vchigir12@gmail.com>
2019-06-24 17:05:57 +0000

    Finished air current object and functionality.
    I opted for using two different settings for the water vapour and the player instead of keeping a list of all objects in contact with the current and updating all separately. It seems to be the best approach for us since we won't have many objects interacting with the air currents.

--- Assets/AirCurrentController.cs
+++ Assets/AirCurrentController.cs
@@ -4,12 +4,70 @@ using UnityEngine;
 
 public class AirCurrentController : MonoBehaviour
 {
-    public float force;
+    [Header("Properties")]
+    [Tooltip("Force Applied to Player by the Air Current")]
+    public float forceP;
+    [Tooltip("Force applied to Water Vapours by the Air Current")]
+    public float forceV;
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private bool active; 
+    private float tempVeolictyY;
+    private Rigidbody2D collidedBody;
+
+    private float resetVelocityTimer;
+    private bool resetVelocity;
+
+    private void Start()
     {
-        if (other.tag == "Player" || other.tag == "VAPOUR") {
-            other.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, force), ForceMode2D.Impulse);
+        active = false;
+        tempVeolictyY = 0;
+        collidedBody = null;
+    }
+
+    private void Update()
+    {
+        if (active) {
+            
+            tempVeolictyY += forceP;
+            collidedBody.velocity = new Vector2(collidedBody.velocity.x, tempVeolictyY);
+
         }
+        else{
+            active = false;
+            collidedBody = null;
+        }
+        if (resetVelocityTimer >= 0.2) {
+            tempVeolictyY = 0;
+            resetVelocity = false;
+            resetVelocityTimer = 0;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (resetVelocity) {
+            resetVelocityTimer += Time.fixedDeltaTime;
+        }
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "WaterVapour") {
+            collision.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, forceV), ForceMode2D.Impulse);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Player") {
+            active = true;
+            collidedBody = other.GetComponent<Rigidbody2D>();
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        resetVelocity = true;
+        active = false;
     }
 }
