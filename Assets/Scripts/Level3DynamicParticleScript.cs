d055883266b01cd7f0c2bea0abdfb1e6927a1ec8
vic <vchigir12@gmail.com>
2019-06-25 21:11:00 +0000

    Finished freezing mechanic

--- Assets/Scripts/Level3DynamicParticleScript.cs
+++ Assets/Scripts/Level3DynamicParticleScript.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Level3DynamicParticleScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private void Start()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnCollisionEnter2D(Collision2D collision)
     {
-        
+        if (collision.transform.tag == "FreezeCollider") {
+            this.transform.GetChild(0).gameObject.SetActive(false);
+            this.GetComponent<SpriteRenderer>().enabled = true;
+            this.GetComponent<BoxCollider2D>().enabled = true;
+            this.transform.localScale = new Vector3(1, 1, 1);
+        }
     }
 }
