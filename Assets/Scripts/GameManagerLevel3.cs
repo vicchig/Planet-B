de1389afab49c8670efb48f7b285f74c8a13fe7a
vic <vchigir12@gmail.com>
2019-06-25 13:53:02 +0000

    Level 3 GameManager and UIManager set up

---
+++ Assets/Scripts/GameManagerLevel3.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerLevel3 : GameManager
+{
+    //[Header("Level 1 Echo Dialogue")]
+
+    [Header("Level 1 Variables")]
+    public int waterNeededInPool1;
+    public int waterNeededInPool2;
+
+    //[Header("Level Objects")]
+
+
+    private int waterInPool1;
+    private int waterInPool2;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        waterInPool1 = 0;
+        waterInPool2 = 0;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+    }
+
+    protected override void changeObjectives()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void checkEchoCollisions()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    protected override void levelEchoMsgChecks()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public int getWaterNeededInPool1() {
+        return waterNeededInPool1;
+    }
+
+    public int getWaterNeededInPool2()
+    {
+        return waterNeededInPool2;
+    }
+
+    public int getWaterInPool1() {
+        return waterInPool1;
+    }
+
+    public int getWaterInPool2()
+    {
+        return waterInPool2;
+    }
+
+    public void setWaterInPool1(int waterAmnt) {
+        waterInPool1 = waterAmnt;
+    }
+
+    public void setWaterInPool2(int waterAmnt)
+    {
+        waterInPool2 = waterAmnt;
+    }
+}
---
+++ Assets/Scripts/UI/UIManagerL3.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManagerL3 : UIManager
+{
+
+    private ProgressBar waterBar1;
+    private ProgressBar waterBar2;
+    private GameManagerLevel3 manager;
+
+
+    protected override void Start()
+    {
+        base.Start();
+        manager = managerObj.GetComponent<GameManagerLevel3>();
+
+        waterBar1 = this.transform.GetChild(3).GetComponent<ProgressBar>();
+        waterBar2 = this.transform.GetChild(6).GetComponent<ProgressBar>();
+
+        setInitialBarValues(waterBar1, 0, manager.getWaterNeededInPool1());
+        setInitialBarValues(waterBar2, 0, manager.getWaterNeededInPool2());
+
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        waterBar1.BarValue = manager.getWaterInPool1();
+        waterBar2.BarValue = manager.getWaterInPool2();
+    }
+
+    
+
+
+}
