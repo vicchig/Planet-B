ff2c3cf481a57efdbed0796bde90c16df2e67ea1
vic <vchigir12@gmail.com>
2019-06-25 21:22:29 +0000

    Melting

--- Assets/Scripts/Level3DynamicParticleScript.cs
+++ Assets/Scripts/Level3DynamicParticleScript.cs
@@ -4,10 +4,23 @@ using UnityEngine;
 
 public class Level3DynamicParticleScript : MonoBehaviour
 {
+    public float heatEnergyThreshold;
+    public float heatEnergyIncrement;
 
     private void Start()
     {
 
+    }
+
+    private void Update()
+    {
+        if (heatEnergyThreshold <= 0) {
+            this.GetComponent<SpriteRenderer>().enabled = false;
+            this.GetComponent<BoxCollider2D>().enabled = false;
+            this.GetComponent<CircleCollider2D>().enabled = true;
+            this.transform.localScale = new Vector3(5, 5, 1);
+            this.transform.GetChild(0).gameObject.SetActive(true);
+        }
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -16,7 +29,13 @@ public class Level3DynamicParticleScript : MonoBehaviour
             this.transform.GetChild(0).gameObject.SetActive(false);
             this.GetComponent<SpriteRenderer>().enabled = true;
             this.GetComponent<BoxCollider2D>().enabled = true;
+            this.GetComponent<CircleCollider2D>().enabled = false;
             this.transform.localScale = new Vector3(1, 1, 1);
+        }
+        else if (collision.transform.tag == "bullet2") {
+            Debug.Log("Getting heated");
+            heatEnergyThreshold -= heatEnergyIncrement;
+            Destroy(collision.gameObject);
         }
     }
 }
