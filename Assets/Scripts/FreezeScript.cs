5131a3e09e9ca461a6d5f7c61b1f4cfbab293213
vic <vchigir12@gmail.com>
2019-06-25 20:39:13 +0000

    Adding freezing mechanic

---
+++ Assets/Scripts/FreezeScript.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FreezeScript : MonoBehaviour
+{
+    public Sprite freezeImage;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.tag == "Player") {
+            this.GetComponent<SpriteRenderer>().sprite = freezeImage;
+        }
+    }
+}
