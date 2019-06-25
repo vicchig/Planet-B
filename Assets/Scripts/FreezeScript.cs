e4518ec1bcdb39230a49ba976513777bace6808a
vic <vchigir12@gmail.com>
2019-06-25 20:44:25 +0000

    No Comments

--- Assets/Scripts/FreezeScript.cs
+++ Assets/Scripts/FreezeScript.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class FreezeScript : MonoBehaviour
 {
+    [Header("Attributes")]
+    [Tooltip("Sprite to change to when object freezes")]
     public Sprite freezeImage;
+    [Tooltip("Tag of the collider which will freeze this object on collision")]
+    public string freezeColliderTag;
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.transform.tag == "Player") {
+        if (collision.transform.tag == freezeColliderTag) {
             this.GetComponent<SpriteRenderer>().sprite = freezeImage;
         }
     }
