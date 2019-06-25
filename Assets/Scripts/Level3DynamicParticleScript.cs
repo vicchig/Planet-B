7d31a31731282dbd467699f826ff234cf03877f0
vic <vchigir12@gmail.com>
2019-06-25 22:54:37 +0000

    Seeping mechanic

--- Assets/Scripts/Level3DynamicParticleScript.cs
+++ Assets/Scripts/Level3DynamicParticleScript.cs
@@ -6,59 +6,79 @@ public class Level3DynamicParticleScript : MonoBehaviour
 {
     public float heatEnergyThreshold;
     public float heatEnergyIncrement;
+    public GameObject seepingCheckpoint;
+    public LayerMask groundLayer;
+    public bool seepingEnabled;
 
     CircleCollider2D cc;
+    BoxCollider2D bc;
+    SpriteRenderer sr;
     private Rigidbody2D rb;
+    private bool seeped;
     // Start is called before the first frame update
     void Start()
     {
         cc = GetComponent<CircleCollider2D>();
         rb = GetComponent<Rigidbody2D>();
+        bc = GetComponent<BoxCollider2D>();
+        sr = GetComponent<SpriteRenderer>();
+        seeped = false;
     }
 
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.name == ("SeepingCollider"))
+        if (collision.transform.tag == "bullet2")
         {
-            cc.enabled = false;
-            Debug.Log(cc.enabled + " enter");
-        }
-        else if(collision.transform.tag == "bullet2")
-        {
-            Debug.Log("Getting heated");
             heatEnergyThreshold -= heatEnergyIncrement;
             Destroy(collision.gameObject);
+        }
+        else if (collision.gameObject.name == "SeepingCollider") {
+            cc.enabled = false;
+            bc.enabled = false;
         }
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
-        if (collision.gameObject.name == ("SeepingCollider"))
+        if (collision.gameObject.name == "SeepingCollider")
         {
             cc.enabled = true;
-            Debug.Log(cc.enabled + " exit");
+            bc.enabled = false;
         }
     }
 
     private void Update()
     {
         if (heatEnergyThreshold <= 0) {
-            this.GetComponent<SpriteRenderer>().enabled = false;
-            this.GetComponent<BoxCollider2D>().enabled = false;
-            this.GetComponent<CircleCollider2D>().enabled = true;
+            sr.enabled = false;
+            bc.enabled = false;
+            cc.enabled = true;
             this.transform.localScale = new Vector3(5, 5, 1);
             this.transform.GetChild(0).gameObject.SetActive(true);
         }
+
+        if (Physics2D.OverlapCircle(seepingCheckpoint.transform.position, 0.1f, groundLayer))
+        {
+            if (rb.position.y > -2)
+            {
+                rb.velocity = new Vector2(rb.velocity.x, -0.5f);
+            }
+            seeped = true;
+        }
+        else if(seepingEnabled && seeped){
+            cc.enabled = true;
+        }
+        
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.transform.tag == "FreezeCollider") {
             this.transform.GetChild(0).gameObject.SetActive(false);
-            this.GetComponent<SpriteRenderer>().enabled = true;
-            this.GetComponent<BoxCollider2D>().enabled = true;
-            this.GetComponent<CircleCollider2D>().enabled = false;
+            sr.enabled = true;
+            bc.enabled = true;
+            cc.enabled = false;
             this.transform.localScale = new Vector3(1, 1, 1);
         }
     }
