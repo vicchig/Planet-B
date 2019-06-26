c4d4c8fcb0aae39f55603e9e55e0d316a3af79f5
vic <vchigir12@gmail.com>
2019-06-26 01:31:36 +0000

    Added mechanic for filling the right water pool

--- Assets/Scripts/Level3DynamicParticleScript.cs
+++ Assets/Scripts/Level3DynamicParticleScript.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Level3DynamicParticleScript : MonoBehaviour
 {
@@ -15,6 +16,10 @@ public class Level3DynamicParticleScript : MonoBehaviour
     SpriteRenderer sr;
     private Rigidbody2D rb;
     private bool seeped;
+    Tilemap destructMap;
+    BoundsInt bounds;
+    int[] destroyed;
+    private bool stopMoving;
     // Start is called before the first frame update
     void Start()
     {
@@ -23,30 +28,15 @@ public class Level3DynamicParticleScript : MonoBehaviour
         bc = GetComponent<BoxCollider2D>();
         sr = GetComponent<SpriteRenderer>();
         seeped = false;
+
+        destructMap = GameObject.Find("Grid").transform.GetChild(3).GetComponent<Tilemap>();
+        bounds = destructMap.cellBounds;
+        destroyed = new int[3];
+        stopMoving = false;
     }
 
 
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
-        if (collision.transform.tag == "bullet2")
-        {
-            heatEnergyThreshold -= heatEnergyIncrement;
-            Destroy(collision.gameObject);
-        }
-        else if (collision.gameObject.name == "SeepingCollider") {
-            cc.enabled = false;
-            bc.enabled = false;
-        }
-    }
 
-    private void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.gameObject.name == "SeepingCollider")
-        {
-            cc.enabled = true;
-            bc.enabled = false;
-        }
-    }
 
     private void Update()
     {
@@ -68,19 +58,105 @@ public class Level3DynamicParticleScript : MonoBehaviour
         }
         else if(seepingEnabled && seeped){
             cc.enabled = true;
+            if (!stopMoving) {
+                checkBlockingWallDestruction();
+            }
+            
         }
-        
+
+
     }
+
+
+
+    private void checkBlockingWallDestruction()
+    {
+        TileBase[] tiles = destructMap.GetTilesBlock(bounds);
+
+        for (int i = 0; i < destructMap.size.x; i++)
+        {
+            for (int j = 0; j < destructMap.size.y; j++)
+            {
+                TileBase tile = tiles[i + j * bounds.size.x];
+                if (tile == null && i == 6)
+                {
+                    if (j == 19 && destroyed[0] == 0)
+                    {
+                        destroyed[0] = 1;
+                    }
+                    else if (j == 20 && destroyed[1] == 0)
+                    {
+                        destroyed[1] = 1;
+                    }
+                    else if (j == 21 && destroyed[2] == 0)
+                    {
+                        destroyed[2] = 1;
+                    }
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < destroyed.Length; i++)
+        {
+            if (destroyed[i] == 1)
+            {
+                count++;
+            }
+        }
+
+        if (count >= 3)
+        {
+            rb.velocity = new Vector2(2, rb.velocity.y);
+        }
+    }
+
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.transform.tag == "FreezeCollider") {
+        if (collision.transform.tag == "FreezeCollider")
+        {
             this.transform.GetChild(0).gameObject.SetActive(false);
             sr.enabled = true;
             bc.enabled = true;
             cc.enabled = false;
             this.transform.localScale = new Vector3(1, 1, 1);
         }
+
     }
 
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.transform.tag == "bullet2")
+        {
+            heatEnergyThreshold -= heatEnergyIncrement;
+            Destroy(collision.gameObject);
+        }
+
+        if (seepingEnabled) {
+            if (collision.gameObject.name == "SeepingCollider")
+            {
+                cc.enabled = false;
+                bc.enabled = false;
+            }
+            else if (collision.gameObject.name == "RightPoolBoundary")
+            {
+                stopMoving = true;
+                rb.velocity = new Vector2(0, 0);
+            }
+        }
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (seepingEnabled) {
+            if (collision.gameObject.name == "SeepingCollider")
+            {
+                cc.enabled = true;
+                bc.enabled = false;
+            }
+        }
+       
+    }
 }
