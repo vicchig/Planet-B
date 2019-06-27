ba233072523e3e6cc4bbcbb9e2c880138a7d7ca1
vic <vchigir12@gmail.com>
2019-06-27 14:12:40 +0000

    Set up scripts and platforms for Level 4

---
+++ Assets/Scripts/GameManagerLevel4.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManagerLevel4 : GameManager
+{
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+    }
+
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+    }
+
+    protected override void changeObjectives()
+    {
+    }
+
+    protected override void checkEchoCollisions()
+    {
+    }
+
+    protected override void levelEchoMsgChecks()
+    {
+    }
+}
