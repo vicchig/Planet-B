02e9a34d856f9e899a2f5ff220942ee45578c192
vic <vchigir12@gmail.com>
2019-06-28 19:29:33 +0000

    Added hurt effect(does not track whether player is taking damage yet)

---
+++ Assets/Scripts/HurtEffectScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtEffectScript : MonoBehaviour
+{
+    public Texture hurtTexture;
+
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        
+    }
+
+    private void OnGUI()
+    {
+        GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), hurtTexture, ScaleMode.StretchToFill);
+    }
+}
