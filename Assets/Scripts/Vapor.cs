c5e8dea28a9a1a49adc35d82af38045f087433b8
Faaris Ali <faarisali111@gmail.com>
2019-06-28 20:48:43 +0000

    added vapor position resetting upon falling

--- Assets/Scripts/Vapor.cs
+++ Assets/Scripts/Vapor.cs
@@ -7,5 +7,29 @@ public class Vapor : MonoBehaviour
 {
     public ParticleSystem steam;
     public ParticleSystem rain;
-    
+
+    public GameObject platform;
+
+    private Rigidbody2D rb;
+    Vector3 startPosition;
+    private void Start()
+    {
+        startPosition = transform.position;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "DeathCollider")
+        {
+            if (platform != null)
+            {
+                transform.position = platform.transform.position + new Vector3(0, 1f, 0);
+            } else
+            {
+                transform.position = startPosition;
+            }
+            rb.velocity = new Vector2(0, 0);
+        }
+    }
 }
