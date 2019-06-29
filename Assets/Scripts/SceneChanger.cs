9feaa4fb1254ee6cddb78641cb31d1478cf151bb
Faaris Ali <faarisali111@gmail.com>
2019-06-29 20:59:14 +0000

    fixed pause menu bugs

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -16,6 +16,7 @@ public class SceneChanger : MonoBehaviour
 
     public void LoadScene(string sceneName) {
         SceneManager.LoadScene(sceneName);
+        Time.timeScale = 1f;
     }
 
     public void quit()
