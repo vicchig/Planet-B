45918c947f16362e3aeef5229871bfc9c767e913
vic <vchigir12@gmail.com>
2019-06-29 18:58:53 +0000

    Made rain last longer in Level 5

--- Assets/Scripts/GameManagerLevel5.cs
+++ Assets/Scripts/GameManagerLevel5.cs
@@ -12,7 +12,6 @@ public class GameManagerLevel5 : GameManager, ILevelManagerCondensation, ILevelM
     public AudioClip objectiveClip5_3;
     public AudioClip objectiveClip5_4;
     public AudioClip objectiveClip5_5;
-    public AudioClip objectiveClip5_6;
 
     [Header("Level 5 Variables")]
     public int condensationNeeded = 5;
@@ -39,7 +38,6 @@ public class GameManagerLevel5 : GameManager, ILevelManagerCondensation, ILevelM
     EchoMessage objectiveLevelTxt5_3;
     EchoMessage objectiveLevelTxt5_4;
     EchoMessage objectiveLevelTxt5_5;
-    EchoMessage objectiveLevelTxt5_6;
 
     protected override void Start()
     {
@@ -55,10 +53,9 @@ public class GameManagerLevel5 : GameManager, ILevelManagerCondensation, ILevelM
         objectiveLevelTxt5_0 = new EchoMessage("Now you have to implement the entire cycle on your own. Let's see what you remember!", objectiveClip5_0, 1);
         objectiveLevelTxt5_1 = new EchoMessage("You have finished the collection part of the water cycle. But what comes next?", objectiveClip5_1, 1);
         objectiveLevelTxt5_2 = new EchoMessage("You have successfully evaporated water from surface bodies of water, but remember that some of the evaporated water has to come from some other source.", objectiveClip5_2, 1);
-        objectiveLevelTxt5_3 = new EchoMessage("Now we just need to make sure the water we evaporated comes back to the ground.", objectiveClip5_3, 1);
+        objectiveLevelTxt5_3 = new EchoMessage("Now we just need to make sure the water we evaporated comes back to the ground. Time to explore the mountain to the west to find a way up into the atmosphere.", objectiveClip5_3, 1);
         objectiveLevelTxt5_4 = new EchoMessage("You have completed the water cycle! The condensed water vapour falls back onto the ground and the cycle begins anew with collection, followed by evaporation/transpiration and finishing with condensation.", objectiveClip5_4, 1);
         objectiveLevelTxt5_5 = new EchoMessage("You have successfully initiated the process of transpiration! But the majority of the evaporated water has to come from some other source.", objectiveClip5_5, 1);
-        objectiveLevelTxt5_6 = new EchoMessage("You have evaporated enough water! Time to explore the mountain to the west to find a way up into the atmosphere.", objectiveClip5_6, 1);
 
         echo.addMessage(objectiveLevelTxt5_0);
 
@@ -89,7 +86,34 @@ public class GameManagerLevel5 : GameManager, ILevelManagerCondensation, ILevelM
 
     protected override void levelEchoMsgChecks()
     {
-       // throw new System.NotImplementedException();
+        if (waterInPool >= waterNeededInPool && !echo.containsMessage(objectiveLevelTxt5_1) && !objectiveLevelTxt5_1.maxTextShowsReached())
+        {
+            echo.addMessage(objectiveLevelTxt5_1);
+        }
+
+        //finished transpiration before pool evaporation
+        if (transpirationAmount >= transpirationNeeded && evaporationAmount < evaporationNeeded && !echo.containsMessage(objectiveLevelTxt5_5) && !objectiveLevelTxt5_5.maxTextShowsReached())
+        {
+            echo.addMessage(objectiveLevelTxt5_5);
+        }
+
+        //finished evaporation from pool before transpiration
+        if (evaporationAmount >= evaporationNeeded && transpirationAmount < transpirationNeeded && !echo.containsMessage(objectiveLevelTxt5_2) && !objectiveLevelTxt5_2.maxTextShowsReached())
+        {
+            echo.addMessage(objectiveLevelTxt5_2);
+        }
+
+        //finished evaporation
+        if ((evaporationAmount + transpirationAmount) >= totalEvaporationNeeded && !echo.containsMessage(objectiveLevelTxt5_3) && !objectiveLevelTxt5_3.maxTextShowsReached())
+        {
+            echo.addMessage(objectiveLevelTxt5_3);
+        }
+
+        //finished condensation
+        if (condensedVapourAmount >= condensationNeeded && !echo.containsMessage(objectiveLevelTxt5_4) && !objectiveLevelTxt5_4.maxTextShowsReached())
+        {
+            echo.addMessage(objectiveLevelTxt5_4);
+        }
     }
 
     protected override void checkEchoCollisions()
