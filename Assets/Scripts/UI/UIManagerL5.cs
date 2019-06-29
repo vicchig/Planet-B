72f1442ecb830c571dc3f3b5b95ca19def6f6ef6
vic <vchigir12@gmail.com>
2019-06-29 00:59:34 +0000

    Added all necessary UI elements to Level 5

--- Assets/Scripts/UI/UIManagerL5.cs
+++ Assets/Scripts/UI/UIManagerL5.cs
@@ -1,18 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class UIManagerL5 : MonoBehaviour
+public class UIManagerL5 : UIManager
 {
-    // Start is called before the first frame update
-    void Start()
+    private ProgressBar waterInPoolBar;
+    private ProgressBar evaporationBar;
+    private ProgressBar condensationBar;
+    private Text waterCount;
+    private Text treeCount;
+
+    private GameManagerLevel5 manager;
+
+    protected override void Start()
     {
-        
+        base.Start();
+
+        manager = managerObj.GetComponent<GameManagerLevel5>();
+
+        waterInPoolBar = this.transform.GetChild(3).GetComponent<ProgressBar>();
+        evaporationBar = this.transform.GetChild(7).GetComponent<ProgressBar>();
+        condensationBar = this.transform.GetChild(4).GetComponent<ProgressBar>();
+
+        waterCount = this.transform.GetChild(1).GetComponent<Text>();
+        treeCount = this.transform.GetChild(6).GetComponent<Text>();
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Update()
     {
-        
+        base.Update();
+
+
     }
 }
