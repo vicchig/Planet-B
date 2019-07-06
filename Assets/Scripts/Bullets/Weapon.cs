8eee66323cdccb9b96bca684f7a7f585e34bda93
Faaris Ali <faarisali111@gmail.com>
2019-07-06 00:21:40 +0000

    removed weapons 2 and 3

--- Assets/Scripts/Bullets/Weapon.cs
+++ Assets/Scripts/Bullets/Weapon.cs
@@ -10,18 +10,18 @@ public class Weapon : MonoBehaviour
 
     [Header("Bullets")]
     public Bullet bulletPrefab;
-    public Bullet bullet1, bullet2, bullet3;
-    public GameObject flameThrower;
-    ParticleSystem ps;
+    //public Bullet bullet1, bullet2, bullet3;
+    //public GameObject flameThrower;
+    //ParticleSystem ps;
 
-    bool isBullet2 = false;
+    //bool isBullet2 = false;
     private PlayerAttributes playerStats;
     // Start is called before the first frame update
     void Start()
     {
-        ps = flameThrower.GetComponent<ParticleSystem>();
-        ps.Stop();
-        bulletPrefab = bullet1;
+        //ps = flameThrower.GetComponent<ParticleSystem>();
+        //ps.Stop();
+        //bulletPrefab = bullet1;
         playerStats = gameObject.GetComponent<PlayerAttributes>();
     }
 
@@ -34,56 +34,56 @@ public class Weapon : MonoBehaviour
             Shoot();
 
         }
-        if (Input.GetButtonDown("Weapon 1"))
-        {
-            bulletPrefab = bullet1;
-            isBullet2 = false;
-        }
-        else if (Input.GetButtonDown("Weapon 2"))
-        {
-            bulletPrefab = bullet2;
-            isBullet2 = true;
-        }
-        else if (Input.GetButtonDown("Weapon 3"))
-        {
-            bulletPrefab = bullet3;
-            isBullet2 = false;
-        }
-        if (Input.GetButtonUp("ShootWeapon"))
-        {
-            ps.Stop();
-        }
-        if (isBullet2)
-        {
-            if (Input.GetButton("ShootWeapon"))
-            {
-                Shoot();
-            }
-        }
+        //if (Input.GetButtonDown("Weapon 1"))
+        //{
+        //    bulletPrefab = bullet1;
+        //    isBullet2 = false;
+        //}
+        //else if (Input.GetButtonDown("Weapon 2"))
+        //{
+        //    bulletPrefab = bullet2;
+        //    isBullet2 = true;
+        //}
+        //else if (Input.GetButtonDown("Weapon 3"))
+        //{
+        //    bulletPrefab = bullet3;
+        //    isBullet2 = false;
+        //}
+        //if (Input.GetButtonUp("ShootWeapon"))
+        //{
+        //    ps.Stop();
+        //}
+        //if (isBullet2)
+        //{
+        //    if (Input.GetButton("ShootWeapon"))
+        //    {
+        //        Shoot();
+        //    }
+        //}
     }
 
     void Shoot()
     {
-        if (isBullet2)
-        {
-            if (playerStats.getCurrentHeat() > 0)
-            {
-                if (Input.GetButtonDown("ShootWeapon"))
-                {
-                    ps.Play();
-                }
-                PlayerMovement player = GameObject.Find("Player3").GetComponent<PlayerMovement>();
-                Bullet bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-                bullet.playerDir = player.dir;
-                playerStats.SetCurrentHeat(playerStats.getCurrentHeat() - 1);
-            } else
-            {
-                ps.Stop();
-            }
-        } else
-        {
-            Bullet bullet = Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
-        }
+        //if (isBullet2)
+        //{
+        //    if (playerStats.getCurrentHeat() > 0)
+        //    {
+        //        if (Input.GetButtonDown("ShootWeapon"))
+        //        {
+        //            ps.Play();
+        //        }
+        //        PlayerMovement player = GameObject.Find("Player3").GetComponent<PlayerMovement>();
+        //        Bullet bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        //        bullet.playerDir = player.dir;
+        //        playerStats.SetCurrentHeat(playerStats.getCurrentHeat() - 1);
+        //    } else
+        //    {
+        //        ps.Stop();
+        //    }
+        //} else
+        //{
+     Bullet bullet = Instantiate(bulletPrefab, firePoint.position, Quaternion.identity);
+        //}
 
     }
 }
