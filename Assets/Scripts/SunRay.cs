27a465e14be6462702db464ba14cfa074ca29c23
Faaris Ali <faarisali111@gmail.com>
2019-07-07 23:25:04 +0000

    SunRay goes to ice child 0

--- Assets/Scripts/SunRay.cs
+++ Assets/Scripts/SunRay.cs
@@ -1,16 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SunRay : MonoBehaviour
 {
     Rigidbody2D rb;
+    public Transform reflectTarget;
+    float speed = 5f;
+    GameObject iceParent;
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         rb.velocity = -transform.right.normalized * 3f;
         Destroy(gameObject, 5f);
+
+        if (SceneManager.GetActiveScene().name == "Level 3")
+        {
+            iceParent = GameObject.Find("IceParent");
+        }
     }
 
     // Update is called once per frame
@@ -26,19 +35,22 @@ public class SunRay : MonoBehaviour
         }
         if (collision.tag == "Player")
         {
-            GameObject gb = GameObject.Find("test");
-            GoTo((Vector2)gb.transform.position);
+            if (SceneManager.GetActiveScene().name == "Level 3")
+            {
+                reflectTarget = iceParent.transform.GetChild(0);
+            }
+            GoTo(reflectTarget.position);
         }
     }
 
     private void GoTo(Vector2 pos)
     {
-        Vector2 v1 = rb.velocity.normalized;
-        Vector2 v2 = (pos - (Vector2)transform.position).normalized;
-        //Debug.Log(Vector2.Dot(v1, v2));
-        float angle = Mathf.Acos(Vector2.Dot(v1, v2));
-        transform.Rotate(new Vector3(0,0,angle*180/Mathf.PI));
-        //Debug.Log(angle);
-        rb.velocity = (pos - (Vector2)transform.position).normalized * 5f;
+        Vector2 newVelocity = (pos - (Vector2)transform.position).normalized;
+
+        rb.velocity = newVelocity * speed;
+
+        float angle = Mathf.Atan2(pos.y - transform.position.y, pos.x - transform.position.x);
+        transform.eulerAngles = new Vector3(0f,0f, angle * Mathf.Rad2Deg);
+
     }
 }
