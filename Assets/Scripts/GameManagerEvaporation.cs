569cbf7c06de65ba47f987c2eb49d83059917287
vic <vchigir12@gmail.com>
2019-07-20 21:53:49 +0000

    Finished splitting GameManagers for level 1

---
+++ Assets/Scripts/GameManagerEvaporation.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class GameManagerEvaporation : GameManager, ILevelManagerWater
+{
+    ///<summary> Amount of water needed in the pool to begin evaporation. </summary>
+    public int waterNeededInPool;
+    public GameObject waterDropParent;
+    public GameObject movingPlatParent;
+
+    protected RisingSteamManager steamManager;
+
+    /// <summary> Script that controls how the player can pour water.</summary>
+    protected WaterPourController waterPControl;
+
+    /// <summary> Current amount of water in the pool, each water droplet is equivalent to 4 of these</summary>
+    protected int amountOfWaterInPool;
+
+    /// <summary> Amount of water that has been evaporated from the pool.</summary>
+    protected int amountOfEvaporatedWater;
+
+
+    //water objects
+    protected GameObject waterParentL;
+    protected GameObject bubbleParent;
+
+    protected GameObject waterParentS;
+
+    protected override void Start()
+    {
+        base.Start();
+
+        amountOfWaterInPool = 0;
+        amountOfEvaporatedWater = 0;
+
+        nextLevelMarker.SetActive(false);
+
+        attributes = player.GetComponent<PlayerAttributes>();
+        waterPControl = player.GetComponent<WaterPourController>();
+
+        steamManager = GameObject.Find("RisingSteam").GetComponent<RisingSteamManager>();
+        waterParentL = GameObject.Find("WaterPoolL");
+        waterParentS = GameObject.Find("WaterPoolS");
+        bubbleParent = GameObject.Find("BubbleParent");
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        //fill pool with water
+        if (attributes.isInPool() && attributes.GetCurrentWater() >= 1)
+        {
+            amountOfWaterInPool += attributes.GetCurrentWater() * 3;
+
+            attributes.SetCurrentWater(0);
+            AudioManager.playSplash();
+        }
+
+        //turn on small and large water pools
+        if (amountOfWaterInPool >= waterNeededInPool)
+        {
+            for (int i = 0; i < waterParentS.transform.childCount; i++)
+            {
+                waterParentL.transform.GetChild(i).gameObject.SetActive(true);
+                waterParentS.transform.GetChild(i).gameObject.SetActive(false);
+            }
+        }
+        else if (amountOfWaterInPool >= 1)
+        {
+            for (int i = 0; i < waterParentS.transform.childCount; i++)
+            {
+                waterParentS.transform.GetChild(i).gameObject.SetActive(true);
+            }
+        }
+
+        //turn on bubbles
+        if (amountOfEvaporatedWater >= steamManager.waterThreshold)
+        {
+            for (int i = 0; i < bubbleParent.transform.childCount; i++)
+            {
+                bubbleParent.transform.GetChild(i).gameObject.SetActive(true);
+            }
+        }
+    }
+
+    protected override void FixedUpdate()
+    {
+        base.FixedUpdate();
+    }
+
+    protected override void changeObjectives()
+    {
+        if (attributes.GetCurrentWater() * 3 >= waterNeededInPool && !(amountOfWaterInPool >= waterNeededInPool))
+        {
+            objectiveDisplay.text = "Current Objective: Fill the empty lake with the collected water.";
+        }
+        else if (amountOfEvaporatedWater >= GameObject.Find("RisingSteam").GetComponent<RisingSteamManager>().waterThreshold)
+        {
+            objectiveDisplay.text = "Current Objective: Go to the marker on the far right side to proceed.";
+        }
+        else if (amountOfWaterInPool >= waterNeededInPool)
+        {
+            objectiveDisplay.text = "Current Objective: Evaporate the water in the lake using heat energy.";
+        }
+    }
+
+    public void SetWaterInPool(int amount)
+    {
+        amountOfWaterInPool = amount;
+    }
+
+    public void SetEvaporatedWater(int amount)
+    {
+        if (amountOfWaterInPool >= waterNeededInPool && amountOfEvaporatedWater < steamManager.waterThreshold)
+        {
+            amountOfEvaporatedWater = amount;
+        }
+
+    }
+
+    public int GetWaterInPool()
+    {
+        return amountOfWaterInPool;
+    }
+
+    public int GetEvaporatedWater()
+    {
+        return amountOfEvaporatedWater;
+    }
+
+    public int GetWaterNeededInPool()
+    {
+        return waterNeededInPool;
+    }
+
+    public int GetEvaporationNeeded()
+    {
+        return 0;
+    }
+}
--- Assets/Scripts/GameManagerLevel1_1.cs
+++ Assets/Scripts/GameManagerLevel1_1.cs
@@ -1,11 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 
-/// <summary> MonoBehaviour for controlling level 1 and tracking player progress in level 1. </summary>
-public class GameManagerLevel1 : GameManagerEvaporation
+public class GameManagerLevel1_1 : GameManagerEvaporation
 {
+
     [Header("Level 1 Echo Dialogue")]
     public AudioClip objectiveLevelClip1_0;
     public AudioClip objectiveLevelClip1_1;
@@ -24,39 +23,39 @@ public class GameManagerLevel1 : GameManagerEvaporation
     EchoMessage objectiveLevelTxt1_6;
     EchoMessage objectiveLevelTxt1_7;
 
-
     protected override void Start()
     {
         base.Start();
 
         //LEVEL OBJECTIVES AUDIO 
-        objectiveLevelTxt1_0 = new EchoMessage("If we want to colonize this planet, we will need to fix its water cycle first. The first step is to evaporate water from surface bodies of water into the atmosphere.", objectiveLevelClip1_0, 1);
-        objectiveLevelTxt1_1 = new EchoMessage("This empty lake would be a perfect source of evaporation if it had water in it. We can use groundwater to fill it, explore the cave system below us. The process of groundwater seeping into lakes and rivers is known as collection. We will talk about it more later.", objectiveLevelClip1_1, 1);
-        objectiveLevelTxt1_2 = new EchoMessage("By my calculations we will need about 14 of these to have enough water to evaporate. You can see the current amount on your HUD or look at the bar that appears near you when you collect water.", objectiveLevelClip1_2, 1);
+        objectiveLevelTxt1_0 = new EchoMessage("You did some good work evaporating the water. But that was not enough, we need to evaporate some more.", objectiveLevelClip1_0, 1);
+        objectiveLevelTxt1_1 = new EchoMessage("This empty lake would be a perfect source of evaporation if it had water in it. We can use groundwater to fill it, explore the cave system below us.", objectiveLevelClip1_1, 1);
         objectiveLevelTxt1_3 = new EchoMessage("We should have enough water now. Head back to the surface and fill the lake with it.", objectiveLevelClip1_3, 1);
         objectiveLevelTxt1_5 = new EchoMessage("Now we just need to evaporate the water. This can be done using heat energy. We will need to find a source.", objectiveLevelClip1_5, 1);
         objectiveLevelTxt1_6 = new EchoMessage("Use your force shield to reflect the sunrays to heat the water.", objectiveLevelClip1_6, 1);
-        objectiveLevelTxt1_7 = new EchoMessage("Congratulations! We have fixed the first stage of the water cycle. As water is heated by the sun, it evaporates in small amounts and rises in the atmosphere.", objectiveLevelClip1_7, 1);
+        objectiveLevelTxt1_7 = new EchoMessage("Congratulations! We have evaporated even more water into the atmosphere. As water is heated by the sun, it evaporates in small amounts and rises in the atmosphere.", objectiveLevelClip1_7, 1);
 
 
         echo.addMessage(objectiveLevelTxt1_0);
         echo.addMessage(objectiveLevelTxt1_1);
     }
 
+
     protected override void Update()
     {
         base.Update();
     }
-    
+
     protected override void FixedUpdate()
     {
         base.FixedUpdate();
     }
 
     /**
-        <summary> Checks whether specific checkpoints in the level have been reached and performs the appropriate action. </summary> 
+     <summary> Checks whether specific checkpoints in the level have been reached and performs the appropriate action. </summary> 
     */
-    protected override void levelEchoMsgChecks() {
+    protected override void levelEchoMsgChecks()
+    {
         //check for whether the pool has enough water to begin evaporation
         if (amountOfWaterInPool >= waterNeededInPool && !objectiveLevelTxt1_5.maxTextShowsReached() && !objectiveLevelTxt1_6.maxTextShowsReached() && !echo.containsMessage(objectiveLevelTxt1_6) && !echo.containsMessage(objectiveLevelTxt1_5))
         {
@@ -65,24 +64,16 @@ public class GameManagerLevel1 : GameManagerEvaporation
         }
 
         //evaporated enough water
-        if (amountOfWaterInPool>= waterNeededInPool && amountOfEvaporatedWater >= GameObject.Find("RisingSteam").GetComponent<RisingSteamManager>().waterThreshold && !objectiveLevelTxt1_7.maxTextShowsReached() && !echo.containsMessage(objectiveLevelTxt1_7))
+        if (amountOfWaterInPool >= waterNeededInPool && amountOfEvaporatedWater >= GameObject.Find("RisingSteam").GetComponent<RisingSteamManager>().waterThreshold && !objectiveLevelTxt1_7.maxTextShowsReached() && !echo.containsMessage(objectiveLevelTxt1_7))
         {
             steamManager.EnableSteam();
             echo.addMessage(objectiveLevelTxt1_7);
             nextLevelMarker.SetActive(true);
         }
-
-        //first water picked up
-        if (attributes.GetCurrentWater() > 0 && !objectiveLevelTxt1_2.maxTextShowsReached() && !echo.isBusy() && attributes.GetCurrentWater() <= 1) {
-            echo.addMessage(objectiveLevelTxt1_2);
-        }   
     }
 
-    /**
-        Check whether Echo is colliding with any of the special collision areas and play the appropriate message. 
-    */
-    protected override void checkEchoCollisions() {
-        
+    protected override void checkEchoCollisions()
+    {
     }
 
     protected override void changeObjectives()
@@ -91,7 +82,7 @@ public class GameManagerLevel1 : GameManagerEvaporation
 
         if (attributes.GetCurrentWater() * 3 < waterNeededInPool && amountOfWaterInPool <= 0)
         {
-            objectiveDisplay.text = "Current Objective: Collect 14 groundwater droplets.";
+            objectiveDisplay.text = "Current Objective: Collect 9 groundwater droplets.";
         }
     }
 }
