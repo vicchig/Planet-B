e62897a1e1e8b96339376b72a6fd0c6ec3ce55ec
vic <vchigir12@gmail.com>
2019-07-22 21:10:10 +0000

    Changed the level 1 water pool platforms so that it is easier to get out of it

---
+++ Assets/Scripts/UI/MenuButtonSoundSource.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuButtonSoundSource : MonoBehaviour
+{
+    private AudioSource source;
+
+    private void Start()
+    {
+        source = new AudioSource();
+    }
+}
